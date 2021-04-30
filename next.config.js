f7a9d0f4a1cef9467f665a003405a86da5ad5093
kiki <luoyu54321@gmail.com>
2021-05-01 00:05:13 +0800

    feat/base/edit next config env setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
   basePath: '/react-next-blog-with-tracking-test',
-  assetPrefix: '/react-next-blog-with-tracking-test'
+  assetPrefix: !debug ? '/react-next-blog-with-tracking-test' : '',
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
+
+    return config
+  }
 }
