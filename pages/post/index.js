349ce0fa9ccaa0135bb6516fd90739b215d2271c
kiki <luoyu54321@gmail.com>
2021-05-01 10:10:57 +0800

    feat/base/add link to dynamic page

--- pages/post/index.js
+++ pages/post/index.js
@@ -3,15 +3,17 @@ import Link from 'next/link'
 
 export default function Home() {
   const customData = require('./data.json');
-  console.log(customData)
+
   return (
     <ErrorBoundary>
       {customData.map((post, index) => {
-        <li key={index}>
-          <Link href={`/${encodeURIComponent(Object.keys(post))}`}>
-            <a>{Object.keys(post)}</a>
-          </Link>
-        </li>
+        return (
+          <li key={index}>
+            <Link href={`/post/${encodeURIComponent(Object.keys(post))}`}>
+              <a>{Object.keys(post)}</a>
+            </Link>
+          </li>
+        )
       })}
       Hey this is post page
     </ErrorBoundary>
