139de6e14369e1f9c5cede4b26dc77dce25a7afb
kiki <luoyu54321@gmail.com>
2021-05-01 11:38:45 +0800

    feat/base/run function in CI

---
+++ component/getPost.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+var path = require("path");
+var marked = require("marked");
+
+const getPost = () => {
+  console.log('create post json file')
+  const postConfig = []
+  const postsDirectory = path.join(process.cwd(), '/pages/post/postContent')
+  const fileNames = fs.readdirSync(postsDirectory)
+  fileNames.map((fileName, index) => {
+    const id = fileName.replace(/\.md$/, '')
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const htmlResult = marked(fileContents);
+    postConfig.push({ [id]: htmlResult })
+  })
+  fs.writeFile("pages/post/data.json", JSON.stringify(postConfig), function (err, result) {
+    if (err) console.log('error', err);
+  })
+}
+
+module.exports.getPost = getPost();
