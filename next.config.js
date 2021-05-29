19151b78a5d3b2a7edc06575acd3a213f0470215
kiki <luoyu54321@gmail.com>
2021-05-29 18:50:07 +0800

    fix/fix ci error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  basePath: '/',
-  assetPrefix: !debug ? 'https://blog.kkluo.com' : '',
+  basePath: '',
+  assetPrefix: !debug ? 'https://blog.kkluo.com/' : '',
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
