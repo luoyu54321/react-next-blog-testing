cc6428a514a4f4120171971e067e943a4dc59d13
kiki <luoyu54321@gmail.com>
2021-06-10 12:38:23 +0800

    feat/base/調整取得文章內容的流程

--- component/getPostContent.js
+++ component/getPostContent.js
@@ -2,50 +2,24 @@ var fs = require("fs");
 var path = require("path");
 var marked = require("marked");
 
-const getFirstElementContent = (string, ele) => {
-  const eleStartIndex = string.indexOf(`<${ele}`);
-  if (eleStartIndex > -1) {
-    const startElementTagLength = `<${ele}`.length;
-    const eleEndIndex = string.indexOf(`/${ele}>`);
-    const elementString = string.slice(eleStartIndex + startElementTagLength, eleEndIndex);
-    const contentStartIndex = elementString.indexOf(">") + 1;
-    const contentEndtIndex = elementString.indexOf("<");
-    const elementContent = elementString.slice(contentStartIndex, contentEndtIndex);
-    return elementContent
-  } else {
-    return ""
-  }
-}
-
 const getPostContent = () => {
   const clientSidePostConfig = []
   const postsDirectory = path.join(process.cwd(), '/pages/note/postContent')
   const fileNames = fs.readdirSync(postsDirectory)
   fileNames.map((fileName, index) => {
-    //TODO-2 : 這裡只負責把資料塞進去就好不需要整理
     const id = fileName.replace(/\.md$/, '')
     const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
     const htmlResult = marked(fileContents);
-    const h1Content = getFirstElementContent(htmlResult, "h1")
-    const h6Content = getFirstElementContent(htmlResult, "h6")
-    let page
-    if (index === 0) {
-      page = Math.floor((index) / 5)
-    } else {
-      page = Math.floor((index - 1) / 5)
-    }
     clientSidePostConfig.push({
       [id]: {
-        "htmlResult": htmlResult,
-        "title": h1Content,
-        "tag": h6Content,
-        "page": page
+        "htmlResult": htmlResult
       }
     })
   })
   fs.writeFile("pages/note/data.json", JSON.stringify(clientSidePostConfig), function (err, result) {
-    if (err) console.log('error', err);
+    if (err) console.log('Post file gernerated failed');
+    if (result) console.log('Post file gernerated succeed')
   })
 }
 
--- pages/api/getPostList.js
+++ pages/api/getPostList.js
@@ -3,5 +3,22 @@
 //TODO-3 : 負責把一堆資料 filter 成 note page 想要的資訊是這裡的工作
 export default (req, res) => {
   const { tag, page } = req.query
+  const h1Content = getFirstElementContent(htmlResult, "h1")
+  const h6Content = getFirstElementContent(htmlResult, "h6")
   res.status(200).json({ tag: tag, page: page })
+}
+
+const getFirstElementContent = (string, ele) => {
+  const eleStartIndex = string.indexOf(`<${ele}`);
+  if (eleStartIndex > -1) {
+    const startElementTagLength = `<${ele}`.length;
+    const eleEndIndex = string.indexOf(`/${ele}>`);
+    const elementString = string.slice(eleStartIndex + startElementTagLength, eleEndIndex);
+    const contentStartIndex = elementString.indexOf(">") + 1;
+    const contentEndtIndex = elementString.indexOf("<");
+    const elementContent = elementString.slice(contentStartIndex, contentEndtIndex);
+    return elementContent
+  } else {
+    return ""
+  }
 }
